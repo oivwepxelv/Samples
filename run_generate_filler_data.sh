25a0ed202aef19ec95b98ca77b88b8bdd39572ae
oivwepxelv <amm.demaio@gmail.com>
2022-04-06 22:13:55 -0400

    rerun

--- run_generate_filler_data.sh
+++ run_generate_filler_data.sh
@@ -1,5 +1,5 @@
 poetry run python generate_filler_data.py \
---impressions_range '[110, 120]' \
+--impressions_range '[1100, 1200]' \
 --reach_percentage_range '[75, 90]' \
---clicks_range '[0, 50]' \
+--clicks_range '[0, 500]' \
 --uclicks_percentage_range '[75, 90]'
